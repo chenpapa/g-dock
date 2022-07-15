7112d62d6f84cd615916df64a5191f2e321d0e40
chopperman <529953602@qq.com>
2022-07-16 07:54:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 修改 argon 为默认主题
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 git clone https://github.com/vernesong/OpenClash.git
 mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
